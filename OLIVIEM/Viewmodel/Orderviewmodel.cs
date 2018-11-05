100953664e42ff9053cc415362965184e2196e80
Karin <knngvyen@gmail.com>
2018-11-05 18:17:58 +0100

    Views toegevoegd

---
+++ OLIVIEM/Viewmodel/Orderviewmodel.cs
@@ -0,0 +1,25 @@
+﻿using Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace OLIVIEM.Viewmodel
+{
+    public enum Status
+    {
+        Verwerken,
+        Verpakken,
+        Verzonden,
+        Bezorgd
+    }
+    public class OrderViewModel
+    {
+        public class Order
+        {
+            public DateTime Date { get; set; }
+            public int Ordernumber { get; set; }
+            public List<Product> Products { get; set; }
+        }
+    }
+}
---
+++ OLIVIEM/Viewmodel/Productviewmodel.cs
@@ -0,0 +1,13 @@
+﻿using Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace OLIVIEM.Viewmodel
+{
+    public class Productviewmodel
+    {
+        public List<Product> products { get; set; }
+    }
+}
